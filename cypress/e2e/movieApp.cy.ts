6df1e685699ff86560c572c54c42a76a7a17433c
emiliamassing <emiliamassings@gmail.com>
2023-02-07 15:15:55 +0100

    ✅ added test to check if elements are created

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -2,15 +2,15 @@ beforeEach(() => {
   cy.visit("/");
 });
 
-describe('Tests for input type text', () => {
+describe('Tests for input field', () => {
 
   it('Should check if input exists', () => {
     cy.get('input').should('exist');
   });
 
   it('Should be able to type in input', () => {
-    cy.get('input').should('exist');
-    cy.get('input').type('Star Wars').should('have.value', 'Star Wars');
+    cy.get('input#searchText').should('exist');
+    cy.get('input#searchText').type('Star Wars').should('have.value', 'Star Wars');
   });
 
 });
@@ -18,15 +18,31 @@ describe('Tests for input type text', () => {
 describe('Tests for searching for a movie', () => {
 
   it('Should find movie', () => {
-    cy.get('input').type('Star Wars').should('have.value', 'Star Wars');
-    cy.get('button').click();
+    cy.get('input#searchText').type('Star Wars').should('have.value', 'Star Wars');
+
+    cy.get('button#search').click();
   });
 
   it('Should not find movie', () => {
-    cy.get('input').type(' ').should('have.value', ' ');
-    cy.get('button').click();
+    cy.get('input#searchText').type(' ').should('have.value', ' ');
+
+    cy.get('button#search').click();
 
     cy.get('p').contains('Inga sökresultat att visa').should('exist');
+  });
+
+});
+
+describe('Tests for when movie is found', () => {
+
+  it('Should create correct elements', () => {
+    cy.get('input#searchText').type('Star Wars').should('have.value', 'Star Wars');
+
+    cy.get('form#searchForm').submit();
+
+    cy.get('div.movie').should('exist');
+    cy.get('div.movie > h3').should('exist');
+    cy.get('div.movie > img').should('exist');
   });
 
 });
