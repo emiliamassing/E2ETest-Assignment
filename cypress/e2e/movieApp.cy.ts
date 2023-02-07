ac3b3443ba8026bf88ec5dc618b79759e15bd0c9
emiliamassing <emiliamassings@gmail.com>
2023-02-07 13:28:24 +0100

    ✅Added tests for when searching for movies

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -14,3 +14,19 @@ describe('Tests for input type text', () => {
   });
 
 });
+
+describe('Tests for searching for a movie', () => {
+
+  it('Should find movie', () => {
+    cy.get('input').type('Star Wars').should('have.value', 'Star Wars');
+    cy.get('button').click();
+  });
+
+  it('Should not find movie', () => {
+    cy.get('input').type(' ').should('have.value', ' ');
+    cy.get('button').click();
+
+    cy.get('p').contains('Inga sökresultat att visa').should('exist');
+  });
+
+});
