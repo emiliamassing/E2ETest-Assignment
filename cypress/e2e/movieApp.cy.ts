b23a042462afcb872f3a09a487bbe1e3c5f90465
emiliamassing <emiliamassings@gmail.com>
2023-02-07 13:15:12 +0100

    ✅Added two tests for input field

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -5,7 +5,11 @@ beforeEach(() => {
 describe('Tests for input type text', () => {
 
   it('Should check if input exists', () => {
-    cy.get('input#searchText').should("exist");
+    cy.get('input').should('exist');
   });
 
+  it('Should be able to type in input', () => {
+    cy.get('input').should('exist');
+    cy.get('input').type('Go to the stable').should('have.value', 'Go to the stable');
+  });
 });
