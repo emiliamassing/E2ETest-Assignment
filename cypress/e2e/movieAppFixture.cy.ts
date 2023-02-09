416e6ef0750012642ab36cb0e04af17fa771f941
emiliamassing <emiliamassings@gmail.com>
2023-02-09 11:48:20 +0100

    ✅Added test for fixture

--- cypress/e2e/movieAppFixture.cy.ts
+++ cypress/e2e/movieAppFixture.cy.ts
@@ -1,3 +1,15 @@
 beforeEach(() => {
     cy.visit("/");
-  });
+});
+
+describe('Tests for finding movies with fixture', () => {
+
+    it('Should get url correctly', () => {
+        cy.intercept('GET', 'http://omdbapi.com/?apikey=145f57ef&s=*', {fixture: "omdb"}).as('omdbFixture');
+
+        cy.get('input#searchText').type('Attack Of The Clones');
+        cy.get('button').click();
+
+        cy.wait('@omdbFixture').its('request.url').should('contain', 'Attack', 'Of', 'The', 'Clones');
+    });
+});
